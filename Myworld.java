7fc22925c69e0c157b8c02b4a6326355479ce222
unknown <exam@siesgst.local>
2023-11-02 09:46:45 +0530

    first commit

---
+++ Myworld.java
@@ -0,0 +1,20 @@
+public class Myworld {
+
+   public static void main(String[] args) {
+        
+      int num1 = 5, num2 = 15, sum,a,b;
+      sum = num1 + num2;
+	
+      System.out.println("Sum of these numbers: "+sum);
+
+		//accepting user input
+	//a=Integer.parseInt(args[0]);
+	//b=Integer.parseInt(args[1]);
+	//sum = a + b;
+	//System.out.println(" num 1: "+a);
+	//System.out.println(" num 2: "+b);
+System.out.println("Sum of user input numbers: "+sum);
+   }
+}
+
+C:\Users\exam\AppData\Local\Programs\Python\Python36-32
